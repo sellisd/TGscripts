e91e9f8b45f06eb9f38ee6069ba809a75e0c7aeb
Diamantis Sellis <dsellis@stanford.edu>
2013-11-03 21:17:28 -0800

    filterCognates.pl ignores rows with X

--- filterCognates.pl
+++ filterCognates.pl
@@ -5,7 +5,7 @@ use Encode;
 use feature 'unicode_strings';
 use Unicode::Normalize;
 binmode(STDOUT, ":utf8");
-my $input = '../data/TG_cognates.13.10.12.20.22.csv';
+my $input = '../data/TG_cognates.13.11.03.csv';
 my $output = $input;
 $output =~s/\.csv/.noIND.csv/;
 #filter cognates file removing all rows that have IND tag
@@ -13,10 +13,9 @@ open IN, '<:encoding(UTF-8)',$input or die $!;
 open OUT, '>:encoding(UTF-8)',$output or die $!;
 while (my $line = <IN>){
     my @ar = split "\t", $line;
-    unless ($ar[0] =~ /IND/){
-	print OUT $line;
-    }
-
+    next if($ar[0] =~ /IND/); #filter by Tag
+    next if($ar[1] =~ /.*X$/);#filter Xs
+    print OUT $line;
 }
 close IN;
 close OUT;
