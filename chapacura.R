184e28a601412f953114b39f4687ec5ca2a30ec3
Diamantis Sellis <sellisd@gmail.com>
2015-10-06 12:56:46 +0200

    chapacuran multistate in R

---
+++ chapacura.R
@@ -0,0 +1,50 @@
+# do not read languages as header to preserve the Unicode
+ga <- read.csv("~/projects/tg/chapacuran/chapacura-207-GA.csv", sep = "\t", as.is = TRUE, header = FALSE)
+meanings <- unique(ga[-1, 2])
+if(length(unique(gsub(" ", "", meanings))) != length(meanings)){
+  stop("if space characters are the only difference between two entries, then it is probably something mistyped, e.g. good* vs good *")
+}
+# add ASCII language names as column headers
+languagesU <- ga[1, c(-1, -2, -3)] # unicode languages
+header <- ga[1, ]
+ga <- ga[-1, ]
+header <- iconv(header, to = "ASCII", sub = "")
+names(ga) <- header
+languages <- header[-c(1, 2, 3)] # ASCII representation of language
+if(length(unique(languages)) != length(unique(languagesU))){
+  stop("At least two languages differ only by a Unicode character")
+}
+#languages <- names(ga)[c(-1,-2,-3)] # exclude No, English and Set columns
+multistateM <- matrix(ncol = (1 + length(languages)), nrow = length(meanings))
+matrixRowCounter <- 1
+for(m in meanings){
+  # m <- meanings[1]
+  states <- character(length(languages))
+  counter <- 1
+  for(l in languages){
+    #l <- languages[1]
+    wordV <- ga[which(ga$English==m),l]
+    if(all(wordV == "?")){
+      states[counter] <- "?"
+    }else{
+      wordI <- which(wordV != "-")
+      if(length(wordI) != 1){
+        if(length(wordI) > 0){
+          # multiple words
+          # stop("multiple words!")
+        }else{
+          stop("no words?!")
+        }
+      }else{
+        #state[counter] <- wordI
+      }
+      states[counter] <- paste(wordI, collapse = ",")
+    }
+    counter <- counter + 1
+  }  
+  multistateM[matrixRowCounter,] <- c(m, states)
+  matrixRowCounter <- matrixRowCounter + 1
+}
+multistateDF <- data.frame(multistateM[, -1], row.names = multistateM[, 1])
+names(multistateDF) <- languages
+write.table(multistateDF, file = "~/projects/tg/chapacuran/multistate.tab", sep = "\t", quote = FALSE, row.names = TRUE, col.names = languagesU)
